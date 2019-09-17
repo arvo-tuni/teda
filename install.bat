dd8758700e898c4802c250b6b52e2a9121825dbf
oleg.spakov <oleg.spakov@uta.fi>
2019-09-17 14:30:43 +0300

    blank line in install.bat

--- install.bat
+++ install.bat
@@ -1,15 +1,19 @@
 @echo off
 
+echo.
 echo Installing external tools...
 npm install -g static
 
+echo.
 echo Downloading the projects...
 
 git clone https://github.com/arvo-tuni/teda-server.git
 git clone https://github.com/arvo-tuni/teda-client.git
 
+echo.
 echo Downloading dependencies and builing the projects...
 
+echo.
 echo [server]
 cd test-data-server
 npm install
@@ -17,11 +21,11 @@ npm run build
 
 cd ..
 
+echo.
 echo [client]
 cd test-data-vis
 npm install
 npm run build
 
+echo.
 echo Done!
-
-pause
