a272582ed889aab457ff5282ad71fcd3c2cc8a1b
oleg.spakov <oleg.spakov@uta.fi>
2019-09-17 15:14:38 +0300

    install updated

--- install.bat
+++ install.bat
@@ -2,30 +2,50 @@
 
 echo.
 echo Installing external tools...
+echo.
 call npm install -g static
 
 echo.
 echo Downloading the projects...
+echo.
 
 call git clone https://github.com/arvo-tuni/teda-server.git
 call git clone https://github.com/arvo-tuni/teda-client.git
 
 echo.
-echo Downloading dependencies and building the projects...
+echo Downloading dependencies...
 
 echo.
 echo [server]
 cd teda-server
 call npm install
-call npm run build
-
 cd ..
 
 echo.
 echo [client]
 cd teda-client
 call npm install
+cd ..
+
+echo.
+echo Fixing some issues with Chart.js...
+echo.
+call git pull -f origin master
+
+echo.
+echo Building the projects...
+
+echo.
+echo [server]
+cd teda-server
 call npm run build
+cd ..
+
+echo.
+echo [client]
+cd teda-client
+call npm run build
+cd ..
 
 echo.
 echo Done!
